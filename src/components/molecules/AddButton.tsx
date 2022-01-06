a6317922a209762b005bfc09132f30d8b142546d
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-07 01:13:42 +0900

    feat: todo追加のためのボタンコンポーネントを作成

---
+++ src/components/molecules/AddButton.tsx
@@ -0,0 +1,45 @@
+import { FC, memo } from "react";
+
+type Props = {
+  handleAdd: () => void;
+};
+
+// メモ化されていないコンポーネント
+const AddButtonWithoutMemo: FC<Props> = (props) => {
+  return (
+    <div
+      className="relative h-14 max-w-lg hover:bg-gray-300 transition"
+      onClick={props.handleAdd}
+    >
+      <input
+        className="appearance-none bg-transparent border-none w-full h-full text-gray-700 ml-10 py-2 px-2 leading-tight focus:outline-none focus:bg-gray-150 font font-body font-bold"
+        type="text"
+        disabled
+        placeholder="新しく追加"
+        aria-label="body"
+      />
+      <button
+        className="absolute left-3 top-1/2 -translate-y-1/2 text-teal-500 hover:text-teal-800"
+        type="button"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+// メモ化してエクスポート
+export const AddButton = memo(AddButtonWithoutMemo);
--- src/components/organisims/TodoList.tsx
+++ src/components/organisims/TodoList.tsx
@@ -1,7 +1,25 @@
+import { AddButton } from "components/molecules/AddButton";
 import { Todo } from "components/molecules/Todo";
 import { FC, memo, useCallback, useState } from "react";
 import { TodoType } from "types/todo";
 
+// テスト用
+// const todolist = [
+//   {
+//     value: "朝ごはん食べる",
+//     id: 1,
+//   },
+//   {
+//     value: "昼ごはん食べる",
+//     id: 2,
+//   },
+//   {
+//     value: "夜ごはん食べる",
+//     id: 3,
+//   },
+// ];
+
+// メモ化されていないコンポーネント
 const TodoListWithoutMemo: FC = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
@@ -30,6 +48,17 @@ const TodoListWithoutMemo: FC = () => {
     [todos]
   );
 
+  const handleAdd = useCallback(() => {
+    const newTodo = {
+      value: "",
+      id: new Date().getTime(),
+    };
+
+    const newTodos = [...todos, newTodo];
+
+    setTodos(newTodos);
+  }, [todos]);
+
   return (
     <div>
       <div>
@@ -44,8 +73,10 @@ const TodoListWithoutMemo: FC = () => {
           />
         ))}
       </div>
+      <AddButton handleAdd={handleAdd} />
     </div>
   );
 };
 
+// メモ化してエクスポート
 export const TodoList = memo(TodoListWithoutMemo);
