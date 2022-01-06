613f78a5a104f6975f6baf54091c2c067c58e2d1
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-06 23:13:07 +0900

    feat: fontFamilyを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      body: [
+        "游ゴシック体",
+        "YuGothic",
+        "游ゴシック",
+        "Yu Gothic",
+        "ヒラギノ角ゴ Pro W3",
+        "Hiragino Kaku Gothic Pro",
+        "メイリオ",
+        "Meiryo",
+        "sans-serif",
+      ],
+    },
     extend: {},
   },
   plugins: [],
