1c835d974bbb8cdc74d6421fe6c40e4957bbf06d
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-08 07:15:27 +0900

    style: pageの背景色を設定

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -9,7 +9,7 @@ type Props = {
 // メモ化されていないコンポーネント
 const DefaultLayoutWithoutMemo: FC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-gray-50">
       <Header />
       {children}
       <Footer />
